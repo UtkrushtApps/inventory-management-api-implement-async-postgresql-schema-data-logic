44a7ec84cd6a578c1dcd33dfeea7f518a083b0d1
Naman Muley <naman.g.muley@gmail.com>
2025-08-02 15:53:12 +0530

    Add app/models.py

---
+++ app/models.py
@@ -0,0 +1,6 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Numeric, DateTime, func
+
+Base = declarative_base()
+
+# Define your Product table model here
